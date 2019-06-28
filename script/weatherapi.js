e523b051844383306bca7b27454beff8c8417baa
addytayler <kin17005@byui.edu>
2019-06-27 18:52:04 -0600

    update

--- script/weatherapi.js
+++ script/weatherapi.js
@@ -6,5 +6,11 @@ weatherRequest.send();
 weatherRequest.onload =  function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
-    document.getElementById("current-temp").innerHTML = "";
+    document.getElementById("current-temp").innerHTML = weatherData.main.temp;
+let icon= "http://openweathermap.org/img/w/" + weatherData.weather.icon + ".png"
+let desc = weatherData.weather[0].description;
+document.getElementById("current-img").setAttribute('src', icon);
+document.getElementById("current-img").setAttribute('description', desc);
+
+
 }
