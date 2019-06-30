a226784587dbd70e57cc3ce34d3894ebeef8b406
addytayler <kin17005@byui.edu>
2019-06-29 18:06:31 -0600

    update

--- script/prestonEvents.js
+++ script/prestonEvents.js
@@ -1,18 +1,18 @@
+var section = document.querySelector('main');
 const RequestTownDataURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 const RequestData = new XMLHttpRequest();
-RequestData.open('GET',RequestTownDataUR, true);
-RequestData.responseType = 'json'
+RequestData.open('GET',requestURL);
+RequestData.responseType = 'json';
 RequestData.send();
 
 RequestData.onload = function() {
     let prestonData = RequestData.response;
     let towns = prestonData['towns'];
+    let output = document.querySelector('main');
 
     towns.forEach (town => {
         if (town.name= 'Preston') {
-            document.getElementById('prestonevents').innerHTML = town.events[0];
-            document.getElementById('prestonevents1').innerHTML = town.events[1];
-            document.getElementById('prestonevents2').innerHTML = town.events[2];
+            document.getElementById('prestonevents').innerHTML = town.events;
         }
     })
 }
--- script/weatherapi.js
+++ script/weatherapi.js
@@ -7,14 +7,26 @@ weatherRequest.onload =  function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
 
-    document.getElementById("current-temp").innerHTML = weatherData.list[0].main.temp;
+    var weekday = new Array(7);
+    weekday[0] ="Monday";
+    weekday[1] ="Tuesday";
+    weekday[2] ="Wednesday";
+    weekday[3] ="Thursday";
+    weekday[4] ="Friday";
+    weekday[5] ="Saturday";
+    weekday[6] ="Sunday";
 
-let icon= "http://openweathermap.org/img/w/" + weatherData.list[0].weather[0].icon + ".png"
-let desc = weatherData.list[0].weather[0].description;
+    let count = 1;
 
-console.log(weatherData.list[0].weather[0].description);
-document.getElementById("current-img").setAttribute('src', icon);
-document.getElementById("current-img").setAttribute('description', desc);
-
-
+    for (let i = 0; i < forecastData.list.length; i++) {
+        if (forecastData.list[i].dt_txt.includes("18:00:00")) {
+            document.getElementById('degreeforecast' +count).innerHTML=forecastData.list[i].main.temp_max;
+            document.getElementById('imgforecast'+count).setAttribute('src', "https://openweathermap.org/img/w/"+forecastData.weather[0] + ".png");
+            document.getElementById('dayforecast' +count).innerHTML=weekday[(new Date().getDay()+count-1)%7];
+            count ++;
+    }
+    if (count==6){
+        break;
+    }
+    }
 }
